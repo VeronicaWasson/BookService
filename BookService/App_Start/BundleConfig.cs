2c2086baabd99c70ccfaf44fdc26a14464da5bcd
Mike Wasson <mwasson@microsoft.com>
2014-08-19 12:48:31 -0700

    Remove jQuery

--- BookService/App_Start/BundleConfig.cs
+++ BookService/App_Start/BundleConfig.cs
@@ -8,9 +8,6 @@ namespace BookService
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
